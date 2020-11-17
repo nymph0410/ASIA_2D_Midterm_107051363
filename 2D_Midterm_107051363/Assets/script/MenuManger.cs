a99472b159bd51a27ca2387f6af9d090c6901481
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2020-11-17 16:00:00 +0800

    缺

---
+++ 2D_Midterm_107051363/Assets/script/MenuManger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManger : MonoBehaviour
+{
+  public void StartGame()
+   {
+        print("開始遊戲");
+
+        SceneManager.LoadScene("場景1");
+   }
+  public void QuitGame() 
+   {
+        print("離開遊戲");
+        Application.Quit();
+   }
+}

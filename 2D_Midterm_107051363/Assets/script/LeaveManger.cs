29acaf655a2e6d402ed9835e9ac85460239bc5a7
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2020-11-20 06:21:00 +0800

    完成

---
+++ 2D_Midterm_107051363/Assets/script/LeaveManger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveManger : MonoBehaviour
+{
+    public void NextLeavel(string nameLV)
+    {
+        SceneManager.LoadScene(nameLV);
+    }
+    public void BacktoMene()
+    {
+        SceneManager.LoadScene("選單");
+            
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- 2D_Midterm_107051363/Assets/script/Player.cs
+++ 2D_Midterm_107051363/Assets/script/Player.cs
@@ -1,6 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; //引用介面API
 
 public class Player : MonoBehaviour
 {
- 
+    public GameObject final;
+    
+    public Text textCount;
+
+    public int count;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name =="傳送門")
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "星星")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "星星數量：" + count;
+        }
+    } 
 }
